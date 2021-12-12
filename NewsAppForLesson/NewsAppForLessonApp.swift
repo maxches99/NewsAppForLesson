fb4a74d2def79535c1840288f78b711b4777e54e
Max Chesnikov <m.chesnikov@app-smart.ru>
2021-12-12 19:42:23 +0300

    Initial Commit

---
+++ NewsAppForLesson/NewsAppForLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppForLessonApp.swift
+//  NewsAppForLesson
+//
+//  Created by Максим Чесников on 12.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppForLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
